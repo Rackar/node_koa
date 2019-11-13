210943b80737a51dbc282ed1e9257172d52a7fb2
旭 杨 <27681135@qq.com>
2019-11-13 21:07:07 +0800

    star 后台ok

--- routes/api/star/index.js
+++ routes/api/star/index.js
@@ -6,6 +6,10 @@ var add = async function(ctx, next) {
   let addStarsNum = ctx.request.body.stars;
   let id = ctx.state.user.userid;
   //   let user = await User.findOne({_id: id});
+  let log = {
+    stars: addStarsNum,
+    createdAt: Date.now()
+  };
 
   let result = await User.updateOne(
     {
@@ -13,7 +17,7 @@ var add = async function(ctx, next) {
     },
     {
       $addToSet: {
-        starsLogs: [addStarsNum]
+        starsLogs: [log]
       }
     }
   );
@@ -31,11 +35,21 @@ var total = async function(ctx, next) {
   let user = await User.findOne({ _id: id });
   if (user) {
     let arr = user.starsLogs;
-    var sum = arr.reduce((prev, next, index, array) => prev + next);
+    var sum = arr.length
+      ? arr.reduce((prev, next, index, array) => {
+          console.log(prev);
+          return { stars: prev.stars + next.stars };
+        })
+      : 0;
+    let data = {
+      stars: sum,
+      real_name: user.username,
+      history: arr
+    };
     ctx.body = {
       status: 1,
       msg: "星星总数",
-      data: sum
+      data: data
     };
   }
 };
