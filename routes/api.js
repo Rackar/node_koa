6a8af0ea45689b8c57810d2e629a5ae5280fa165
rackar <27681135@qq.com>
2020-03-28 11:00:10 +0800

    修改

--- routes/api.js
+++ routes/api.js
@@ -19,20 +19,37 @@ var storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 router.post("/image", upload.single("avatar"));
-// router.post("/article", async function(ctx, next) {
-//   let body = ctx.request.body;
-//   var article = await new Article({
-//     content: ctx.request.body.content,
-//     title: ctx.request.body.title
-//   });
-//   article.save();
+const axios = require('axios');
 
-//   ctx.body = {
-//     msg: "新增成功"
-//   };
-// });
+var addLink = async function (ctx, next) {
+  // res.send('respond with a resource');
+  let params = ctx.request.body;
+
+  let url = params.url
+    .split(/\n/)
+    .filter(ele => ele !== "" && ele !== " ");
+  let pass = params.pass;
+  let gzlist = "ilianyue";
+  let push = await axios.post("http://123.206.94.184:5000/add", { url, pass, gzlist });
+  console.log(push.data);
+
+  if (push.data == "ok") {
+    ctx.body = {
+      status: 1,
+      msg: "文章发布成功"
+    };
+  } else {
+    ctx.body = {
+      status: 0,
+      msg: "文章发布失败"
+    };
+  }
+
+};
+
+router.post("/lianyue", addLink);
 
 router.use(person.routes(), person.allowedMethods()); // /person
 router.use(star.routes(), star.allowedMethods()); // /stars
-router.use(lianyue.routes(), lianyue.allowedMethods()); // /person
+// router.use(lianyue.routes(), lianyue.allowedMethods()); // /person
 module.exports = router;
