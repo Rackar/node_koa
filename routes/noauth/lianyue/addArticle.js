1b662d655b90300df924334bf795609ea64b8b0f
Rackar <27681135@qq.com>
2021-06-28 08:00:34 +0800

    chagen ly ip

--- routes/noauth/lianyue/addArticle.js
+++ routes/noauth/lianyue/addArticle.js
@@ -1,30 +1,31 @@
 const router = require("koa-router")();
-const axios = require('axios');
+const axios = require("axios");
 
 var addLink = async function (ctx, next) {
   // res.send('respond with a resource');
   let params = ctx.request.body;
 
-  let url = params.url
-    .split(/\n/)
-    .filter(ele => ele !== "" && ele !== " ");
+  let url = params.url.split(/\n/).filter((ele) => ele !== "" && ele !== " ");
   let pass = params.pass;
   let gzlist = "ilianyue";
-  let push = await axios.post("http://123.206.94.184:5000/add", { url, pass, gzlist });
+  let push = await axios.post("http://175.24.95.47:5003/add", {
+    url,
+    pass,
+    gzlist,
+  });
   console.log(push.data);
 
   if (push.data == "ok") {
     ctx.body = {
       status: 1,
-      msg: "文章发布成功"
+      msg: "文章发布成功",
     };
   } else {
     ctx.body = {
       status: 0,
-      msg: "文章发布失败"
+      msg: "文章发布失败",
     };
   }
-
 };
 
 router.post("/lianyue", addLink);
