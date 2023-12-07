abb8b4bf26ead307207c8824bac61417ee8f3e64
Rackar <27681135@qq.com>
2023-12-07 16:51:00 +0800

    feat: add shp schema

---
+++ models/location/shp.js
@@ -0,0 +1,45 @@
+const mongoose = require("../../api/db_mongoose");
+//var Schema = mongoose.Schema;
+const pointSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ['Point'],
+    required: true
+  },
+  coordinates: {
+    type: [Number],
+    required: true
+  }
+});
+
+const citySchema = new mongoose.Schema({
+  name: String,
+  location: {
+    type: pointSchema,
+    required: true
+  }
+});
+
+const polygonSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ['Polygon'],
+    required: true
+  },
+  coordinates: {
+    type: [[[Number]]], // Array of arrays of arrays of numbers
+    required: true
+  }
+});
+
+const shpSchema = new mongoose.Schema({
+  name: String,   // 批文名称
+  type:String,   // 批文类型
+  areaCode:String,   //行政区划
+  setupDate:String, // 批准日期
+  setupUnit:String, // 批准单位
+  location: polygonSchema
+},
+{ timestamps: true ,strict:false});
+
+module.exports = mongoose.model("Shp", shpSchema);
